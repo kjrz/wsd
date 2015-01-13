9767a2c6e5c417940a8a8edc8b2175f9b8e96dc9
kjrz <kuba.jarz@gmail.com>
2015-01-13 22:12:25 +0100

    Move misplaced car factory methods

--- src/main/java/traffic/Garage.java
+++ src/main/java/traffic/Garage.java
@@ -4,4 +4,12 @@ package traffic;
  * @author kjrz
  */
 public class Garage {
+
+    public Car getTraditionalCar() {
+        throw new UnsupportedOperationException();
+    }
+
+    public Car getSelfDrivingCar() {
+        return null;
+    }
 }
--- src/main/java/traffic/Route.java
+++ src/main/java/traffic/Route.java
@@ -4,12 +4,4 @@ package traffic;
  * @author kjrz
  */
 public class Route {
-
-    public Car getTraditionalCar() {
-        throw new UnsupportedOperationException();
-    }
-
-    public Car getSelfDrivingCar() {
-        return null;
-    }
 }
